2bdc1d935448c8d5c6ab1b1d7b3bb6f835714f98
mattialicausi <licausivitomattia@gmail.com>
2022-11-25 13:54:59 +0100

    Aggiunto php e template in censura.php per censurare la parola inserita in index.php

--- censura.php
+++ censura.php
@@ -1,3 +1,43 @@
 <?php
+
+    //FUNZIONE PER PRENDERE LA PAROLA DA CENSURARE
+
+    if(isset($_GET['textCensura']) && $_GET['textCensura'] != ''){
+        $textCensura = $_GET['textCensura'];
+    } else {
+        $textCensura = 'Inserisci una parola da censurare';
+    }
+
+    //FUNZIONE PER PRENDERE IL TESTO DA CUI CENSURARE LA PAROLA
     
+    if(isset($_GET['text']) && $_GET['text'] != ''){
+        $text = $_GET['text'];
+    } else {
+        $text = 'Inserisci del testo da cui censurare una parola';
+    }
+
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>Bad words response</title>
+</head>
+<body>
+
+    <div class="text-center container">
+        <h1>Bad Words</h1>
+
+        <?php echo '<p>'.$textCensura.'</p>' ?>
+        <?php echo '<p>'.$text.'</p>' ?>
+        
+        
+    </div>
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -6,11 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link rel="stylesheet" href="./css/style.css">
-    <title>Document</title>
+    <title>Bad words</title>
 </head>
 <body>
     <div class="text-center container">
-        <h1>Scrivi un testo e censura una parola</h1>
+        <h1>Bad Words</h1>
         <form class="d-flex flex-column" action="censura.php">
             <input type="text" name="textCensura">
             <textarea name="text" id="text" cols="30" rows="5"></textarea>
